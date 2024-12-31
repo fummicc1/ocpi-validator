6403e3d5bc26ad2ae507d7c0943f800d4dfd5676
fummicc1 <fumiya.tennis1@gmail.com>
2024-12-31 22:21:58 +0900

    Initial Commit

---
+++ Sources/validators/Location.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  ocpi-validator
+//
+//  Created by Fumiya Tanaka on 2024/12/31.
+//
+
+struct LocationValidator {
+    func validate(json: [String: Any]) throws {
+        let requiredFields = [
+            "id",
+
+        ]
+    }
+}
